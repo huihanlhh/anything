e96da638c3b2d643c34e0a91b9b63e71e43c58f9
Huihan Li <huihanli@Huihans-MacBook-Pro.local>
2022-08-24 17:24:19 -0700

    add another method

--- load_pt_and_convert_to_flax.py
+++ load_pt_and_convert_to_flax.py
@@ -1,4 +1,6 @@
 from transformers import FlaxLukeModel, LukeModel
+from transformers.modeling_flax_pytorch_utils import convert_pytorch_state_dict_to_flax
+import jax.numpy as jnp
 
 # Load and save the pt model
 pt_model = LukeModel.from_pretrained("studio-ousia/luke-base", cache_dir="~/.cache/huggingface/transformers/luke")
@@ -13,3 +15,10 @@ flax_model.save_pretrained("~/.cache/huggingface/transformers/luke")
 # This code works great for LUKE.
 # For mLUKE, we might need to make some changes to `modeling_flax_luke.py`,
 # because mLUKE checkpoint does not have entity-aware attention. Here we might need to do some weight renaming or other tricks.
+
+# Another useful function is the following (referring to https://github.com/huggingface/transformers/blob/c55d6e4e10ce2d9c37e5f677f0842b04ef8b73f3/tests/test_modeling_flax_common.py#L271"
+# This might be helpful if we end up having to convert by hand
+pt_model = LukeModel.from_pretrained("studio-ousia/luke-base", cache_dir="~/.cache/huggingface/transformers/luke")
+fx_model = FlaxLukeModel(pt_model.config, dtype=jnp.float32)
+fx_state = convert_pytorch_state_dict_to_flax(pt_model.state_dict(), fx_model)
+fx_model.params = fx_state
